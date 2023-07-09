55b7a998e75ce970dfbe0113bd3a6ec383ead9b4
blackqahwah <67290908+blackqahwah@users.noreply.github.com>
2023-07-09 16:52:29 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import './assets/main.css'
+import 'bootstrap'
+import './assets/bootstrap/dist/css/bootstrap.css'
+import './assets/bootstrap/dist/js/bootstrap.js'
+
+ import { BootstrapVue, BootstrapVueIcons } from './assets/bootstrap-vue'
+
+import './assets/bootstrap-vue/dist/bootstrap-vue.css'
+import './assets/bootstrap-vue/dist/bootstrap-vue.js'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
+
+createApp(App).mount('#app')
