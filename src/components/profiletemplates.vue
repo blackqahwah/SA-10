5a3c3f39aae0b28d95c7aa68c7e4347cd926cf2a
blackqahwah <67290908+blackqahwah@users.noreply.github.com>
2024-01-06 00:45:32 +0000

    now working on profile section

---
+++ src/components/profiletemplates.vue
@@ -0,0 +1,11 @@
+<!-- ChildComponent.vue -->
+<template>
+ <div class="card">
+   <div class="card-title">
+     <slot name="title"></slot>
+   </div>
+   <div class="card-body">
+     <slot name="body"></slot>
+   </div>
+ </div>
+</template>
---
+++ src/components/userprofile.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import ProfileTemplates from './profiletemplates.vue'
+
+</script>
+
+<!-- ParentComponent.vue -->
+<template>
+ <ProfileTemplates>
+   <template v-slot:title>
+     <h1>Title of the Card</h1>
+   </template>
+   <template v-slot:body>
+     <p>Body of the Card</p>
+   </template>
+ </ProfileTemplates>
+</template>
